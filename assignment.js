dd356a86c7f205d9d89cba3adf0dbd317c081881
Ayon-Biswas <ayonshirsho@gmail.com>
2021-01-22 20:21:05 +0600

    hotelcost

--- assignment.js
+++ assignment.js
@@ -1,7 +1,22 @@
 //1.
-function kilometerToMeter(kilo){
-    var meter = kilo*1000;
-    return meter;
+// function kilometerToMeter(kilo){
+//     var meter = kilo*1000;
+//     return meter;
+// }
+// var convert = kilometerToMeter(12);
+// console.warn(convert);
+
+//4.
+var friends = ["arindom", "rudra", "shuvo","sumaiya","ayon"];
+function megaFriend (friends){
+ var name = friends[0];
+ for(var i = 0; i<friends.length;i++){
+ var element = friends[i];
+ if(element > name){
+    name = element;
+ }
+ }
+return name;
 }
-var convert = kilometerToMeter(12);
-console.warn(convert);
+var result = megaFriend(name);
+console.warn(result);
