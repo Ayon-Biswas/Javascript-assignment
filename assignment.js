58c07290fda0f64990b04c7449505415e2db2b0f
Ayon-Biswas <ayonshirsho@gmail.com>
2021-01-22 21:17:50 +0600

    megafriend

--- assignment.js
+++ assignment.js
@@ -6,6 +6,19 @@ function kilometerToMeter(kilo){
 var convert = kilometerToMeter(12);
 console.warn(convert);
 
+//2
+function budgetCalculator(ram, sam, jodu) //ram = clock , sam = mobile , jodu = laptop//
+{
+    var clock = 50 * ram;
+    var mobile = 100 * sam;
+    var laptop = 500 * jodu;
+
+    var total_price = clock + mobile + laptop;
+    return total_price;
+}
+    var result = budgetCalculator(10, 5, 3);
+    console.log(result);
+
 //3.
 function hotelCost(number){
     if(number<=10){
